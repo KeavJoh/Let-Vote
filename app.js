d289609666f0cdccb450b429ecc40da331807abf
Kevin Johr <kevin.johr@posteo.de>
2022-09-11 21:30:12 +0200

    implement data attribute

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
     // Optionen
+    data: function() {
+        return {
+            submissions: submissions //aus seed.js
+        }
+    }
 })
 
 // Liefert die Instanz zur Root-Component zurück
