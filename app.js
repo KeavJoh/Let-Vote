8bc0c49346ac6b27c970a6ce0a7212ff552ab26a
Kevin Johr <kevin.johr@posteo.de>
2022-09-11 22:27:08 +0200

    code formatted

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const app = Vue.createApp({
-    // Optionen
-    data: function() {
-        return {
-            submissions: submissions //aus seed.js
-        }
+  // Optionen
+  data: function () {
+    return {
+      submissions: submissions, //aus seed.js
+    };
+  },
+  methods: {
+    // upvote: function() {}
+    upvote() {
+      this.submissions[0].votes++;
     },
-    methods: {
-        // upvote: function() {}
-        upvote() {
-            this.submissions[0].votes++;
-        },
-    }
-})
+  },
+});
 
 // Liefert die Instanz zur Root-Component zurück
 const vm = app.mount("#app");
