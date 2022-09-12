e50dc46e623ca8a4bf706fed8f084d0da73f47d2
Kevin Johr <kevin.johr@posteo.de>
2022-09-12 23:00:50 +0200

    add total Vote function

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ const app = Vue.createApp({
       submissions: submissions, //aus seed.js
     };
   },
+  computed: {
+    totalVotes() {
+      return this.submissions.reduce((totalVotes, submissions) => {
+        return totalVotes + submissions.votes;
+      }, 0);
+    }
+  },
   methods: {
     // upvote: function() {}
     upvote() {
       this.submissions[0].votes++;
     },
-  },
+  }
 });
 
 // Liefert die Instanz zur Root-Component zurück
