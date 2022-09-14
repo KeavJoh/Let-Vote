89ffec660635313d2bcf090ec87bc49107da4ba9
Kevin Johr <kevin.johr@posteo.de>
2022-09-14 22:25:49 +0200

    template outsourcing

--- app.js
+++ app.js
@@ -20,23 +20,59 @@ const app = Vue.createApp({
       return {
         "bg-primary": this.totalVotes >= 50,
         "text-white": this.totalVotes >= 50,
-      }
+      };
     },
     cardTitleFontSize() {
       return {
-        fontSize: this.totalVotes + "px"
-      }
-    }
+        fontSize: this.totalVotes + "px",
+      };
+    },
   },
   methods: {
     // upvote: function() {}
-    upvote(submissionID) {
+/*     upvote(submissionID) {
       const submission = this.submissions.find(
         (submission) => submission.id === submissionID
       );
       submission.votes++;
-    },
+    }, */
   },
+});
+
+// Globale Component
+app.component("SubmissionListItem", {
+  // Optionen
+  props: ["submission"],
+  methods: {
+    upvote() {
+      this.submission.votes++;
+    }
+  },
+  template: `
+  <div class="d-flex">
+   <div class="d-shrink-0">
+    <img v-bind:src="submission.img" alt="placeholder" />
+   </div>
+   <div class="flex-grow-1 ms-3">
+    <!-- <h5 v-once> -->
+    <h5>
+      {{ submission.title }}
+      <span
+        class="float-end text-primary"
+        style="cursor: pointer"
+        v-on:click="upvote()"
+        ><i class="fa fa-chevron-up"></i
+        ><strong>{{ submission.votes }}</strong></span
+      >
+    </h5>
+    <!-- <div>{{ submissions[0].desc }}</div> -->
+    <div v-html="submission.desc"></div>
+    <small class="text-muted"
+      >Gericht kommt aus: {{ submission.author }}</small
+    >
+   </div>
+  </div>
+  `
 });
 
 // Liefert die Instanz zur Root-Component zurück
